039a99041ed6dc4ae2de30d7421b4b99b30a78fe
rahalrazika <rahalraika@gmail.com>
2021-05-09 00:15:56 +0200

    add prop-types

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ function App() {
         <p>
          Hello world
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
       </header>
     </div>
   );
