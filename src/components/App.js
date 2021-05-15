a0840d3fb5b08426cf64908abddafb6b8a9cb674
rahalrazika <rahalraika@gmail.com>
2021-05-15 22:57:04 +0200

    add handleClick event

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
+import React from 'react';
 import '../App.css';
 import ButtonPanel from './buttonPanel';
 import Display from './display';
 import calculate from '../logic /calculate';
 
-const App = () => (
-  <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operaton: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    <Display />
-    <ButtonPanel onClick={calculate} />
+  handleClick = (buttonName) => {
+    this.setState((prevState) => calculate(prevState, buttonName));
+  }
 
-  </div>
-);
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel onClick={this.handleClick} />
+
+      </div>
+    );
+  }
+}
 
 export default App;
