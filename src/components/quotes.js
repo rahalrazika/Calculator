fa2d6d5f7541871a11f309a5fadcb0dfd62a6b5a
rahalrazika <rahalraika@gmail.com>
2021-06-02 13:26:17 +0200

    add routes to the app

---
+++ src/components/quotes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quotes = () => (
+  <div className="quote-text">
+    <blockquote>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding
+    </blockquote>
+    <cite>William Paul Thurston</cite>
+  </div>
+
+);
+
+export default Quotes;
